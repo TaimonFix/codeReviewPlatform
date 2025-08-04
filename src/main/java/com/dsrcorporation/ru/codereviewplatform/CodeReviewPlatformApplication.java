a308dc9d5732b3ed4aac0a40e487cdcb73c4d0f3
bratyshevTD <bratyshev2013@mail.ru>
2025-08-04 12:27:55 +0300

    Initial commit

---
+++ src/main/java/com/dsrcorporation/ru/codereviewplatform/CodeReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dsrcorporation.ru.codereviewplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeReviewPlatformApplication.class, args);
+    }
+
+}
