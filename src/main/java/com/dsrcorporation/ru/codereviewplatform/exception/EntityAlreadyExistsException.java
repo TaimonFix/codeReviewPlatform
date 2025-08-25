a7f5dfe6d8d8986254839ab780596be4bff10e76
bratyshevTD <bratyshev2013@mail.ru>
2025-08-25 17:26:53 +0300

    feat: разделил backend и frontend

--- src/main/java/com/dsrcorporation/ru/codereviewplatform/exception/EntityAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.dsrcorporation.ru.codereviewplatform.exception;
-
-public class EntityAlreadyExistsException extends RuntimeException {
-    public EntityAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/dsrcorporation/ru/codereviewplatform/service/FileService.java
+++
@@ -1,7 +0,0 @@
-package com.dsrcorporation.ru.codereviewplatform.service;
-
-/**
- * Сервис для работы с файлами.
- */
-public class FileService {
-}
